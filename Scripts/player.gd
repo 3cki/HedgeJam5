c47901e7ab0d728edfa2d24714bcdc8f88eb5ab7
Maximilian Eckert <eckertm2000@gmail.com>
2023-04-08 14:28:15 +0200

    2: feat: torch collision

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,16 +3,15 @@ extends CharacterBody2D
 @export var tile_size = 16
 @export var speed = 3
 @onready var ray = $RayCast2D
+var ray_size = 1
+var inputs = {"Walk Right": Vector2.RIGHT, "Walk Left": Vector2.LEFT, "Walk Up": Vector2.UP, "Walk Down": Vector2.DOWN}
 var tween: Tween
-var torchTween: Tween
-var inputs = {"Walk Right": Vector2.RIGHT,
-			"Walk Left": Vector2.LEFT,
-			"Walk Up": Vector2.UP,
-			"Walk Down": Vector2.DOWN}
+var torch
+var torch_tween: Tween
 var can_pick_up_torch = false
 var picked_up_torch = false
-var position_difference_torch = Vector2.ZERO
-var torch
+var torch_direction = Vector2.ZERO
+var torch_offset = Vector2.ZERO
 
 func _ready():
 	tile_size = tile_size * 5
@@ -30,19 +29,32 @@ func _unhandled_input(event):
 		pick_up_torch()
 
 func move(dir):
+	if torch_movement_restrictions(dir): return
+	update_ray(dir)
+	rotate_sprite(dir)
+	if !ray.is_colliding():
+		var new_player_position = position + inputs[dir] * tile_size
+		move_tween(new_player_position)
+		move_tween_torch(new_player_position)
+		
+func torch_movement_restrictions(dir):
 	if picked_up_torch:
 		if abs(torch.position.x - position.x) > abs(torch.position.y - position.y):
 			if inputs[dir] == Vector2.UP || inputs[dir] == Vector2.DOWN:
-				return
+				return true
 		else:
 			if inputs[dir] == Vector2.LEFT || inputs[dir] == Vector2.RIGHT:
-				return
-	ray.target_position = inputs[dir] * tile_size / 5
+				return true
+		if inputs[dir] == torch_direction:
+			ray_size = 2
+		else:
+			ray_size = 1
+	else:
+		ray_size = 1
+
+func update_ray(dir):
+	ray.target_position = (inputs[dir] * tile_size / 5) * ray_size
 	ray.force_raycast_update()
-	rotate_sprite(dir)
-	if !ray.is_colliding():
-		move_tween(dir)
-		
 
 func check_torch_contact():
 	can_pick_up_torch = false
@@ -61,24 +73,39 @@ func pick_up_torch():
 	if can_pick_up_torch || picked_up_torch:
 		picked_up_torch = !picked_up_torch
 		torch.get_node("CollisionShape2D").disabled = picked_up_torch
-		position_difference_torch = position - torch.position
+		get_torch_direction()
 
-func move_tween(dir):
+func move_tween(new_player_position):
 	if tween:
 		tween.kill
-	if torchTween:
-		tween.kill
 	tween = create_tween()
-	torchTween = create_tween()
-	tween.tween_property(self, "position", position + inputs[dir] * tile_size,
-		0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "position", new_player_position, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_callback(check_torch_contact)
+
+func move_tween_torch(new_player_position):
 	if picked_up_torch:
-		var newTorchPosition = position + inputs[dir] * tile_size - position_difference_torch
-		torchTween.tween_property(torch, "position", newTorchPosition, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		if torch_tween:
+			torch_tween.kill
+		torch_tween = create_tween()
+		var new_torch_position = new_player_position - torch_offset
+		torch_tween.tween_property(torch, "position", new_torch_position, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 
 func rotate_sprite(dir):
 	if dir == "Walk Left":
 		$Sprite.flip_h = true
 	if dir == "Walk Right":
 		$Sprite.flip_h = false
+
+func get_torch_direction():
+	var position_difference = position - torch.position
+	
+	torch_offset = position_difference
+	
+	if abs(position_difference.x) > abs(position_difference.y):
+		position_difference.x = -round(position_difference.x)
+		position_difference.y = 0
+	else:
+		position_difference.x = 0
+		position_difference.y = -round(position_difference.y)
+		
+	torch_direction = position_difference.normalized()
