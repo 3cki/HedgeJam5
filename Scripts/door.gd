2b20fe82805da257fcdf1b7fe0d8347299b1b0b8
Maximilian Eckert <eckertm2000@gmail.com>
2023-04-10 10:36:44 +0200

    4: feat: switch scenes

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
+@export var next_scene: PackedScene
+
 func _ready():
 	add_to_group("door")
 
-
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		print("Player entered")
+		get_tree().change_scene_to_packed(next_scene)
