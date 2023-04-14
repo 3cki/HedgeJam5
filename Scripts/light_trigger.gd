0198d58996181eb08028db7d6beaf93368bffef4
Maximilian Eckert <eckertm2000@gmail.com>
2023-04-14 11:21:11 +0200

    15: feat: finished

--- Scripts/light_trigger.gd
+++ Scripts/light_trigger.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
 @export var group_name = "light_trigger_group"
+@export var initial_color = Color(1, 1, 1, 1)
+@export var light_up_color = Color(1, 1, 0, 1)
 var light_on = false
 
 func _ready():
@@ -12,13 +14,13 @@ func toggle_light():
 	get_node("PointLight2D").enabled = light_on
 	if light_on:
 		get_node("LightOccluder2D").occluder_light_mask = 2
-		$Sprite2D.modulate = Color(1, 1, 0, 1);
+		$Sprite2D.modulate = light_up_color;
 		for el in get_tree().get_nodes_in_group(group_name):
 			el.set_process_mode(PROCESS_MODE_DISABLED)
 			el.visible = false
 	else:
 		get_node("LightOccluder2D").occluder_light_mask = 1
-		$Sprite2D.modulate = Color(1, 1, 1, 1);
+		$Sprite2D.modulate = initial_color;
 		for el in get_tree().get_nodes_in_group(group_name):
 			el.set_process_mode(PROCESS_MODE_ALWAYS)
 			el.visible = true
